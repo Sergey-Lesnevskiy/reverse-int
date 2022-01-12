493ca3bde33107faec34c9d997b606710fddd252
Sergey-Lesnevskiy <sergeybaian@mail.ru>
2022-01-12 20:08:08 +0300

    first node -v 16

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let a=n+"";
+    let rev="";
+    for (let i = a.length-1; i >=0; i--) {
+        rev=rev+a[i];
+    }
+    const result=+rev;
+   
+    return result;
 }
+console.log(reverse(291))
