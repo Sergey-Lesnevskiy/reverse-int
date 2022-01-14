4af7c3eab9c45dac10cd70d3c7c617cb1f8962e9
Sergey-Lesnevskiy <sergeybaian@mail.ru>
2022-01-14 09:42:52 +0300

    happen

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 module.exports = function reverse (n) {
+if (n<0){
+    n=n*(-1);
     let a=n+"";
     let rev="";
     for (let i = a.length-1; i >=0; i--) {
         rev=rev+a[i];
     }
-    const result=+rev;
-   
-    return result;
+    let result=+rev;
+    // let result1 = result*(-1);
+    return result; 
+}else{
+   let a=n+"";
+    let rev="";
+    for (let i = a.length-1; i >=0; i--) {
+        rev=rev+a[i];
+    }
+    let result=+rev;
+return result;
+}
+
 }
